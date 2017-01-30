6cc092e6fdd023501699151bcca40790124923a8
Johan Svensson <j_svensson@me.com>
2017-01-30 17:20:42 +0100

    make a new file and replace stuff in that.
    hardcoded filenames, and so far just for test-files

--- star_replacer.js
+++ star_replacer.js
@@ -1,12 +1,22 @@
 var replace = require("replace")
+var copyFile = require("quickly-copy-file")
 
 var stars = require('./behind_the_stars.json')
 
-for (var key in stars) {
-	string_to_change = "{{3}" + key + "}{3}"
-	replace ({
-		regex: string_to_change, 
-		replacement: stars[key], 
-		paths: ["test.xml"],
-	})
+file_with_stars = "test.xml"
+file_with_secrets = "test_realz.xml"
+
+function makeReplacements(stars){
+	for (var key in stars) {
+		string_to_change = "{{3}" + key + "}{3}"
+		replace ({
+			regex: string_to_change, 
+			replacement: stars[key], 
+			paths: [file_with_secrets],
+		})
+	}
 }
+
+copyFile(file_with_stars, file_with_secrets)
+	.then(() => makeReplacements(stars))
+
